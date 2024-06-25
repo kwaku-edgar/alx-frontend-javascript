d420703cc28e6c2c71b6a8e163f26c0170c17b1b
Agongo Edgar Nyamekye <kwakuedgar@gmail.com>
2024-06-25 01:02:47 +0000

    ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const result = [];
-  for (let value of array) {
-  result.push(appendString + value);
-  }
-
-  return result;
-}
